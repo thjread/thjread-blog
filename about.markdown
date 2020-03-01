3b1059301c158697e3ddbb69cc2fc87e79a4e9d3
Thomas Read <thjread@gmail.com>
2020-03-01 21:11:50 +0000

    Configure CircleCI

---
+++ about.markdown
@@ -0,0 +1,10 @@
+---
+title: About
+---
+I'm currently a third year student studying maths at Trinity College, Cambridge.
+
+Find me on
+
+- GitHub: <https://github.com/thjread>
+- LinkedIn: <https://linkedin.com/in/thjread>
+- Twitter: <https://twitter.com/thjread>
--- site.hs
+++ site.hs
@@ -22,7 +22,7 @@ main = do renderFormulae <- initFormulaCompilerDataURI 1000 defaultEnv
                   route   idRoute
                   compile compressCssCompiler
 
-              match (fromList ["about.rst", "contact.markdown"]) $ do
+              match (fromList ["about.markdown", "contact.markdown"]) $ do
                   route   $ setExtension "html"
                   compile $ pandocCompiler
                       >>= loadAndApplyTemplate "templates/default.html" defaultContext
