69d8cb51ebc75a7049df815fa20ab6e48ce73d1f
Thomas Read <thjread@gmail.com>
2020-03-01 12:14:33 +0000

    Working latex formulae

---
+++ posts/2020-03-01-algebraic-tangent-spaces.markdown
@@ -0,0 +1,45 @@
+---
+title: The algebraic approach to tangent spaces of varieties
+---
+Let $V = \mathbb{V}(I)$ an irreducible affine variety, and let $p \in V$. Let $\mathcal{O}_{V, p}$ be the local ring at $p$, with maximal ideal $m_p$. Then we claim that there is a natural isomorphism of vector spaces
+
+\[T_{V, p} \cong (m_p / m_p^2) \text{.}\]
+
+Let $M_p$ be the maximal ideal of $p$ in $\mathbb{A}^n_k$, and let $\bar{M}_p = M_p / I(V)$ the maximal ideal in $k[V]$.
+
+The coordinate functions $x_1, \dotsc, x_n$ form a basis of $(k^n)^\ast$. For $f \in k[x_1, \dotsc, x_n]$ we have a corresponding linear functional
+
+\[f^{(1)}_p = \sum_{i=1}^n \diffp{f}{x_i}(p) x_i \in (k^n)^\ast \text{.}\]
+
+Hence we obtain a linear map
+
+\[d : M_p \to (k^n)^\ast \quad f \mapsto f^{(1)}_p \text{.}\]
+
+This map is surjective ($d(x_i) = x_i$) and has kernel $M_p^2$, so induces an isomorphism $M_p / M_p^2 \cong (k^n)^\ast$. The inclusion $T_{V, p} \subset k^n$ induces a surjection $(k^n)^\ast \to (T_{V, p})^\ast$ via restriction of functionals, so we get a surjection
+
+\[D : M_p / M_p^2 \to (k^n)^\ast \to (T_{V, p})^\ast \text{.}\]
+
+Now we have
+
+\begin{align*}f \in \ker(D) &\iff f^{(1)}_p \!\mid_{T_{V, p}}\\
+&\iff f^{(1)}_p = g_{p}^{(1)} \quad \text{for some $g \in I(V)$}\\
+&\iff f - g \in M_p^2  \quad \text{for some $g \in I(V)$}\\
+&\iff f \in M_p^2 + I(V)
+\end{align*}
+
+(where the second step follows by the fact that $T_{V, p} \cong \{d(g) \mid g \in I(V)\}^\circ \subset k^n$ so $f^{(1)}_p \!\mid_{T_{V, p}} = 0$ iff $f \in (T_{V, p})^\circ \cong \text{span}\{d(g) \mid g \in I(V)\} = \{d(g) \mid g \in I(V)\} \subset (k^n)^\ast$).
+
+Hence we have
+
+\[\bar{M}_p / \bar{M}_p^2 \cong M_p / (M_p^2 + I(V)) \cong (T_{V, p})^\ast \text{.}\]
+
+So it suffices to show that $\bar{M}_p / \bar{M}_p^2 \cong m_p / m_p^2$. The inclusion $\bar{M}_p \to m_p$ induces an inclusion
+
+\[\varphi : \bar{M}_p / \bar{M}_p^2 \to m_p / m_p^2\]
+
+so suffices to check that $\varphi$ is surjective. Take $f/g \in m_p$, and let $c = g(p) \ne 0$. Then
+
+\[\frac{f}{g} - \frac{f}{c} = \frac{f(c-g)}{gc} \in m_p^2\]
+
+so $\varphi(f/c) = \overline{(f/g)} \in m_p / m_p^2$.
+
--- site.hs
+++ site.hs
@@ -1,62 +1,69 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import           Hakyll.Contrib.LaTeX
+import           Image.LaTeX.Render
+import           Image.LaTeX.Render.Pandoc
+import           Text.Pandoc.Options
+import           Text.Pandoc
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+main = do renderFormulae <- initFormulaCompilerDataURI 1000 defaultEnv
+          hakyll $ do
+              match "images/*" $ do
+                  route   idRoute
+                  compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+              match "css/*" $ do
+                  route   idRoute
+                  compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+              match (fromList ["about.rst", "contact.markdown"]) $ do
+                  route   $ setExtension "html"
+                  compile $ pandocCompiler
+                      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                      >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+              match "posts/*" $ do
+                  route $ setExtension "html"
+                  compile $ customPandocCompiler renderFormulae
+                      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+                      >>= loadAndApplyTemplate "templates/default.html" postCtx
+                      >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+              create ["archive.html"] $ do
+                  route idRoute
+                  compile $ do
+                      posts <- recentFirst =<< loadAll "posts/*"
+                      let archiveCtx =
+                              listField "posts" postCtx (return posts) `mappend`
+                              constField "title" "Archives"            `mappend`
+                              defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+                      makeItem ""
+                          >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                          >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                          >>= relativizeUrls
 
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
+              match "index.html" $ do
+                  route idRoute
+                  compile $ do
+                      posts <- recentFirst =<< loadAll "posts/*"
+                      let indexCtx =
+                              listField "posts" postCtx (return posts) `mappend`
+                              defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+                      getResourceBody
+                          >>= applyAsTemplate indexCtx
+                          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                          >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+              match "templates/*" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
@@ -64,3 +71,28 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+--------------------------------------------------------------------------------
+customPandocCompiler :: (PandocFormulaOptions -> Pandoc -> Compiler Pandoc) -> Compiler (Item String)
+customPandocCompiler renderFormulae =
+   let defaultReaderExtensions = readerExtensions defaultHakyllReaderOptions
+       readerOptions = defaultHakyllReaderOptions {
+         readerExtensions = enableExtension Ext_tex_math_single_backslash defaultReaderExtensions
+       }
+
+       defaultWriterExtensions = writerExtensions defaultHakyllWriterOptions
+       writerOptions = defaultHakyllWriterOptions {
+         writerExtensions = enableExtension Ext_tex_math_single_backslash defaultWriterExtensions
+       }
+       preamble = "\\usepackage{amsmath} \\usepackage{amssymb} \\usepackage{enumerate} \\usepackage{diffcoeff}"
+       displayMathOptions = displaymath {
+         preamble = preamble
+       }
+       mathOptions = math {
+         preamble = preamble
+       }
+       formulaOptions = defaultPandocFormulaOptions {
+         formulaOptions = \case DisplayMath -> displayMathOptions; _ -> mathOptions
+       }
+   in pandocCompilerWithTransformM readerOptions writerOptions
+          (renderFormulae formulaOptions)
