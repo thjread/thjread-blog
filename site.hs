28d75e9a25dbdcb325ff5e0f5f78ebfc08296c0e
Thomas Read <thjread@gmail.com>
2020-03-06 16:35:02 +0000

    Production mode, with prerendered mathjax

--- site.hs
+++ site.hs
@@ -4,84 +4,84 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 import           Text.Pandoc.Options
+import           System.Environment (lookupEnv)
+import           Data.Maybe (isJust)
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+main =
+  do prod <- isJust <$> lookupEnv "PROD"
+     let myDefaultContext =
+           boolField "prod" (const prod) `mappend` defaultContext
+     let myPostCtx = dateField "date" "%B %e, %Y" `mappend`
+           myDefaultContext
+     hakyll $ do
+       match "images/*" $ do
+         route   idRoute
+         compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+       match "css/*" $ do
+         route   idRoute
+         compile compressCssCompiler
 
-    match "CNAME" $ do
-        route   idRoute
-        compile copyFileCompiler
+       match "CNAME" $ do
+         route   idRoute
+         compile copyFileCompiler
 
-    match (fromList ["about.markdown", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+       match (fromList ["about.markdown", "contact.markdown"]) $ do
+         route   $ setExtension "html"
+         compile $ pandocCompiler
+           >>= loadAndApplyTemplate "templates/default.html" myDefaultContext
+           >>= relativizeUrls
 
-    match "posts/**" $ do
-        route $ setExtension "html"
-        compile $ customPandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+       match "posts/**" $ do
+         route $ setExtension "html"
+         compile $ customPandocCompiler
+           >>= loadAndApplyTemplate "templates/post.html"    myPostCtx
+           >>= loadAndApplyTemplate "templates/default.html" myPostCtx
+           >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/**"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+       create ["archive.html"] $ do
+         route idRoute
+         compile $ do
+           posts <- recentFirst =<< loadAll "posts/**"
+           let myArchiveCtx =
+                 listField "posts" myPostCtx (return posts) `mappend`
+                 constField "title" "Archives"              `mappend`
+                 myDefaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+           makeItem ""
+             >>= loadAndApplyTemplate "templates/archive.html" myArchiveCtx
+             >>= loadAndApplyTemplate "templates/default.html" myArchiveCtx
+             >>= relativizeUrls
 
+       match "index.html" $ do
+         route idRoute
+         compile $ do
+           posts <- recentFirst =<< loadAll "posts/**"
+           let myIndexCtx =
+                 listField "posts" myPostCtx (return posts) `mappend`
+                 myDefaultContext
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/**"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
+           getResourceBody
+             >>= applyAsTemplate myIndexCtx
+             >>= loadAndApplyTemplate "templates/default.html" myIndexCtx
+             >>= relativizeUrls
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
-
-    match "templates/*" $ compile templateBodyCompiler
-
-
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+       match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
 customPandocCompiler :: Compiler (Item String)
 customPandocCompiler =
-   let defaultReaderExtensions = readerExtensions defaultHakyllReaderOptions
-       readerOptions = defaultHakyllReaderOptions {
-         readerExtensions = enableExtension Ext_tex_math_single_backslash defaultReaderExtensions
-       }
+  let defaultReaderExtensions = readerExtensions defaultHakyllReaderOptions
+      readerOptions = defaultHakyllReaderOptions {
+        readerExtensions = enableExtension Ext_tex_math_single_backslash defaultReaderExtensions
+        }
 
-       defaultWriterExtensions = writerExtensions defaultHakyllWriterOptions
-       writerOptions = defaultHakyllWriterOptions {
-         writerExtensions = enableExtension Ext_tex_math_single_backslash defaultWriterExtensions,
-         writerHTMLMathMethod = MathJax ""
-       }
-   in pandocCompilerWith readerOptions writerOptions
+      defaultWriterExtensions = writerExtensions defaultHakyllWriterOptions
+      writerOptions = defaultHakyllWriterOptions {
+        writerExtensions = enableExtension Ext_tex_math_single_backslash defaultWriterExtensions,
+          writerHTMLMathMethod = MathJax ""
+        }
+  in pandocCompilerWith readerOptions writerOptions
---
+++ templates/mathjax-options.js
@@ -0,0 +1,16 @@
+{
+    "CommonHTML": {
+        "linebreaks": { "automatic": true }
+    },
+    "HTML-CSS": {
+        "linebreaks": { "automatic": true }
+    },
+    "SVG": {
+        "linebreaks": { "automatic": true }
+    },
+    "TeX": {
+        "Macros": {
+            "diffp": ["{\\frac{\\partial #1}{\\partial #2}}", 2]
+        }
+    }
+}
